e2aea80d90750f54f8d37590410deb45434cfc04
Keira Hodgkison <khodgkison@gmail.com>
2015-03-15 19:41:22 +1100

    Amend home page layout

--- config/routes.rb
+++ config/routes.rb
@@ -2,9 +2,11 @@ Rails.application.routes.draw do
   #devise_for :users
   resources :consultants
 
-  root :to => redirect('/consultants')
+  root :to => redirect('/locate')
 
   get '/locate' => 'locate#locate'
+
+  get '/consultants' => 'consultants#index'
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
